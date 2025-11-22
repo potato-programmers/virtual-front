14fb86d2863ec0cdf4142453a80284d58aaea6c0
hyunwoo0081 <hyunwoo0081@gmail.com>
2025-11-22 22:45:14 +0900

    fix: vite config 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // GitHub Pages 배포를 위한 base 경로 설정
-  // 'virtual-front'를 실제 GitHub 리포지토리 이름으로 변경하세요.
-  base: import.meta.env.PROD ? '/virtual-front/' : '/',
-  build: {
-    outDir: 'dist',
-  },
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+
+  return {
+    base: isProduction ? '/virtual-front/' : '/',
+    build: {
+      outDir: 'dist',
+    },
+  };
 });
